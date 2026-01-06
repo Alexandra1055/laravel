02bdaa859337f22f48e95a6c7fe64c45535d82bd
Alexandra1055 <agonzalez1055@alumnes.politecnicllevant.cat>
2026-01-06 19:49:52 +0100

    about page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::get('/about', function () {
+    return 'About Page';
+});//esto devuelve un string, es un ejemplo simple
+
+ Route::get('/about', function () {
+    return ['foo' => 'bar'];
+}); //esto devuelve un json, para futuras apis, pero no es lo ideal
+
+ */
+
+Route::get('/about', function () {
+    return view('about');
+});

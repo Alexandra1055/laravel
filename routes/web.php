c53cf8926a4b1e92836ef918355cb8592a1fdd05
Alexandra1055 <agonzalez1055@alumnes.politecnicllevant.cat>
2026-01-07 18:39:13 +0100

    variables dentro de las rutas

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        'greeting' => 'Hello!', //pasar datos a la vista
+        'name' => 'Perry the Platypus'
+    ]);
 });
 
 /*Route::get('/about', function () {

6acefc2d75b67708c292f400b36524df7da6c75b
Alexandra1055 <agonzalez1055@alumnes.politecnicllevant.cat>
2026-01-07 19:01:43 +0100

    creamos una clase job para retornar una array

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
+class Job
+{
+    public static function all(): array
+    {
+        return [
+            ['id' => 1, 'title' => 'Director', 'salary' => 50000],
+            ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
+            ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
+        ];
+    }
+}
+
 Route::get('/', function () {
     return view('home');
 });
@@ -17,24 +29,14 @@ Route::get('/', function () {
 
  */
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function (){
     return view('jobs', [
-        'jobs' => [
-            ['id' => 1, 'title' => 'Director', 'salary' => 50000],
-            ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
-            ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
-        ],
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        ['id' => 1, 'title' => 'Director', 'salary' => 50000],
-        ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
-        ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
-    ];
-
-    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
+    $job = Arr::first(Job::all(), fn ($job) => $job['id'] == $id);
 
     return view('job', ['job' => $job]);
 });

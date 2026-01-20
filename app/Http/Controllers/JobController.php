bb28c7ac0f1591e41938b321775c5ea188887a82
Alexandra1055 <agonzalez1055@alumnes.politecnicllevant.cat>
2026-01-20 02:06:22 +0100

    terminamos de implementar vista y envio con la clase mailable

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
 use App\Models\User;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -37,15 +39,18 @@ class JobController extends Controller
             'salary'=>['required']
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
 
+        Mail::to($job->employer->user)->send(
+            new JobPosted($job)
+        );
+
         return redirect('/jobs');
     }
-
     public function edit(Job $job)
     {
         return view('jobs.edit', ['job' => $job]);
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,8 @@
 
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
-use App\Mail\JobPosted;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
-
-Route::get('test', function(){
-    \Illuminate\Support\Facades\Mail::to('jeffrey@laracast.com')->send(
-        new JobPosted()
-    );
-
-    return 'Done';
-});
-
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
